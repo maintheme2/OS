fe257e776c5892b1553e6e85d979a749ee9bd51f
maintheme <oligarh20031@mail.ru>
2022-09-20 23:16:50 +0300

    finished h/w

--- week03/ex1/ex1.c
+++ week03/ex1/ex1.c
@@ -1,13 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+size_t
+foo(short age) {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    size_t year = tm.tm_year + 1900;
+    return year - age;
+}
 
 int32_t
 main(int argc, char const *argv[]) {
     const int32_t x = 10;
     const int32_t* q = &x;
 
-    const int32_t* const p = malloc(5 * sizeof(int32_t));
+    int32_t* const p = malloc(5 * sizeof(int32_t));
+    
     memset(p, x, 5);
-    printf("d", *p);
+    for (short i = 0; i < 5; i++) 
+        printf("%p ", (p+i));
+
+    printf("\n");
+    
+    for (short i = 0; i < 5; i++) {
+        short age;
+        scanf("%hd", &age);
+        *(p+i) = age;
+    }
+
+    for (short i = 0; i < 5; i++) 
+        *(p+i) = foo(*(p+i));
+
+    // for (short i = 0; i < 5; i++) 
+    //     printf("%d ", *(p+i));
+
+    free(p);
     return 0;
 }
