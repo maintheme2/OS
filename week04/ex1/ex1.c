a822bd710280d432f9694636fa20d8906921dccc
maintheme <oligarh20031@mail.ru>
2022-09-22 22:55:56 +0300

    1st part finished

---
+++ week04/ex1/ex1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <time.h>
+
+int 
+main() {
+	pid_t pid1 = fork();
+
+    clock_t t1 = clock();
+
+    if (pid1 == 0) {
+        t1 = clock() - t1;
+        printf("child1: %d, parent: %d\nit took me %lf seconds to finish child1\n",
+        getpid(), getppid(), (double)t1/CLOCKS_PER_SEC);
+
+    } else {
+        pid_t pid2 = fork();
+        clock_t t2 = clock();
+
+        if (pid2 == 0) {
+            t2 = clock() - t2;
+            printf("child2: %d, parent: %d\nit took me %lf seconds to finish child2\n", 
+            getpid(), getppid(), (double)t2/CLOCKS_PER_SEC);
+
+        } else {
+            t1 = clock() - t1;
+            printf("main: %d, parent: %d\nit took me %lf seconds to finish main\n", 
+            getpid(), getppid(), (double)t1/CLOCKS_PER_SEC);
+        }
+    }
+	return EXIT_SUCCESS;
+}
