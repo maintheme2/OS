94527860199db173a68180c335242fa07f0e8694
maintheme2 <oligarh20031@mail.ru>
2022-09-27 21:21:42 +0300

    solved h/w

---
+++ week04/ex4/ex4.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool
+contains(char* str, char ch) {
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == ch)
+            return true;
+    }
+    return false;
+}
+
+int
+main(void) {
+    char cmd_path[256] = "/usr/bin/";
+    char* args[256] = {};
+
+    char* cmd = malloc(sizeof(char*));
+    printf("Enter the command: ");    
+    scanf("%[^\n]", cmd);
+
+    int16_t j = 0;
+    if (contains(cmd, ' ')) {
+        char* token = strtok(cmd, " ");
+        strcat(cmd_path, token);
+        while (token != NULL) {
+            args[j++] = token;
+            token = strtok(NULL, " ");
+        }
+    } else strcat(cmd_path, cmd);
+
+    if (execvp(cmd_path, args) == -1)
+        perror("couldn't compile");
+}
