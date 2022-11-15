b7bdf5baf1d3d6d58def4b9ab75ebdffca8b4412
maintheme2 <oligarh20031@mail.ru>
2022-11-15 18:50:22 +0300

    finished h/w

--- week10/ex3/ex3.sh
+++ week10/ex3/ex3.sh
@@ -2,10 +2,9 @@
 
 touch _ex3.txt
 echo "some content" > _ex3.txt
-chmod o-r _ex3.txt
+chmod -x _ex3.txt
 ls -l _ex3.txt > ex3.txt
-chmod ugo+r _ex3.txt
-chmod ugo-w _ex3.txt
+chmod uo=rwx _ex3.txt
 ls -l _ex3.txt >> ex3.txt
-chmod g=u _ex3.txt
+chmod g=rwx _ex3.txt
 ls -l _ex3.txt >> ex3.txt
